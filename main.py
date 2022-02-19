20e8f8be5d17484bf64c0530916d449228c972ee
Eric Owusu Junior <e.owusu89@gmail.com>
2022-02-19 22:32:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Hello there! Have a good morning",
+                     from_='+19378724705',
+                     to='+19022297944'
+                 )
+
+print(message.sid)
